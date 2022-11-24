d1090e59310aee22c9fc84a09d6d5065a7a939f7
Andrew Kastsou <argusward@gmail.com>
2022-11-24 12:08:58 +0300

    summ

--- multiplicator/multiplicator.cpp
+++ multiplicator/multiplicator.cpp
@@ -1,47 +1,16 @@
 #include <iostream>
 #include <iomanip>
-template<typename T>
-T summ(T x, T y)
+template<typename T, typename K>
+K summ(T x, K y)
 {
-    return x + y;
+    T z=x+y
+    return z;
 }
 int main()
 {
-    char a[40], b[40], c='.';
-    int count=0, k, a1=0, a2=0;
-    std::cout << "Vvedite dva chisla dlya summirovania: " << std::endl;
-    std::cin >> a >> b;
-    for (int i=0; a[i]; i++)
-    {
-        if(a[i]==c)
-        {
-        count++;
-        k=i;
-        }
-    }
-    for (int i=k+1; a[i]; i++)
-    {
-        a1++;
-    }
-    for (int i=0; b[i]; i++)
-    {
-        if(b[i]==c)
-        {
-        count++;
-        k=i;
-        }
-    }
-    for (int i=k+1; b[i]; i++)
-    {
-        a2++;
-    }
-    if (a1>a2)
-    k=a1;
-    else
-    k=a2;
-    if (count>0)
-    std::cout << std::fixed << std::setprecision(k) << "Rezultat: " << summ(std::stof(a),std::stof(b)) << std::endl;
-    else
-    std::cout << "Rezultat: " << summ(std::stoi(a), std::stoi(b)) << std::endl;  
+    std::cout << "Summ : " << summ(2, 3) << std::endl;
+    std::cout << "Summ : " << summ(2.5, 3) << std::endl;
+    std::cout << "Summ : " << summ(2, 3.1) << std::endl;
+    std::cout << "Summ : " << summ(2.5, 2.6) << std::endl;
     return 0;
 }
